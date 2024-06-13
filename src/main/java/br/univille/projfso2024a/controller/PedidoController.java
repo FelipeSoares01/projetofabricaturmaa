717fda3fb940ccdd5938220ea36825205ee86199
FelipeSoares01 <soaresfelipe1992@hotmail.com>
2024-06-12 21:40:00 -0300

    revisão

--- src/main/java/br/univille/projfso2024a/controller/PedidoController.java
+++ src/main/java/br/univille/projfso2024a/controller/PedidoController.java
@@ -1,13 +1,24 @@
 package br.univille.projfso2024a.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.projfso2024a.entity.ItemPedido;
+import br.univille.projfso2024a.entity.Pedido;
 import br.univille.projfso2024a.service.PedidoService;
 import br.univille.projfso2024a.service.ProdutoService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pedidos")
@@ -23,5 +34,59 @@ public class PedidoController {
         var listaPedidos = pedidoService.getAll();
         return new ModelAndView("pedido/index","listaPedidos",listaPedidos);
     }
+    @GetMapping("/novo")
+    public ModelAndView novo(){
+        var pedido = new Pedido();
+        var novoItem = new ItemPedido();
+        var listaProdutos = produtoService.getAll();
+        HashMap<String,Object> dados = new HashMap<>();
+        dados.put("pedido",pedido);
+        dados.put("novoItem",novoItem);
+        dados.put("listaProdutos",listaProdutos);
+         
+        return new ModelAndView("pedido/form", dados);
+    }
+
+    @PostMapping(params = "incitem")
+    public ModelAndView novo(Pedido pedido,
+                            ItemPedido novoItem){
+        pedido.getColItens().add(novoItem);
+        var listaProdutos = produtoService.getAll();
+        HashMap<String,Object> dados = new HashMap<>();
+        dados.put("pedido",pedido);
+        dados.put("novoItem",novoItem);
+        dados.put("listaProdutos",listaProdutos);
+         
+        return new ModelAndView("pedido/form", dados);
+    }
+
+    @PostMapping(params = "save")
+    public ModelAndView novo(@Valid Pedido pedido,
+                            BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            var listaProdutos = produtoService.getAll();
+            var novoItem = new ItemPedido();
+            HashMap<String,Object> dados = new HashMap<>();
+            dados.put("pedido",pedido);
+            dados.put("novoItem",novoItem);
+            dados.put("listaProdutos",listaProdutos);
+            return new ModelAndView("pedido/form", dados);
+        }
+        pedidoService.save(pedido);
+        return new ModelAndView("redirect:/pedidos");
+
+
+    }
+
+
     
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePedido(@PathVariable Long id) {
+        try {
+            pedidoService.deletePedido(id); // Implemente o serviço para excluir o pedido pelo ID
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -17,6 +17,29 @@
         })
     })
 
+    $(document).ready(function(){
+        let servicoId;
+
+        // Evento para abrir o modal e guardar o ID do serviço
+        $(".js-delete").on("click", function(){
+            servicoId = $(this).attr("data-id");
+            $('#modalExcluirServico').modal('show');
+        });
+
+        // Evento para confirmar a exclusão do serviço
+        $("#btnExcluirServico").on("click", function(){
+            $.ajax({
+                url: "/servicos/delete/" + servicoId,
+                method: "GET",
+                success: function(){
+                    window.location.href = "/servicos";
+                },
+                error: function(){
+                    alert("Erro ao excluir o serviço.");
+                }
+            });
+        });
+    });
     // Adicionar evento de clique para o botão "Não"
     $("#modalExcluirCliente").on("click", ".btn-secondary", function() {
         $("#modalExcluirCliente").modal("hide");
